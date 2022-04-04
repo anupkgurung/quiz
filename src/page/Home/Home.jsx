fc2178f13791f75e04a7659c2de7326e1854a186
nealcaffre <31470135+nealcaffre@users.noreply.github.com>
2022-04-04 22:37:13 +0530

    feat : home page added for quiz

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,7 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { QuizCard } from "../../component/QuizCard/QuizCard"
+
+const getCategories = async (setCategories) => {
+    const { data : {categories} } = await axios.get("api/category/");
+    setCategories(categories)
+}
 export const Home = () => {
+    const [categories, setCategories] = useState([]);    
+    useEffect(()=>{getCategories(setCategories)},[setCategories])
     return (
         <>
-            <div>Home</div>
+            <div className="flex-row pd-5 m-5 justify-evenly">
+            {categories.length > 0 && categories.map(({id,cateoryName,description,image,path})=>
+                <QuizCard key={id}
+                    id={id}
+                    cateoryName={""}
+                    description={description}
+                    caption={cateoryName}
+                    image={image}
+                    path={path}
+                 />
+            )}
+            </div>
         </>
     )
 }
